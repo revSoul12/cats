384f76ba7d0870ce17ff14ee0800918f99df8ce1
revSoul12 <daniel.laserna@gmail.com>
2019-07-30 22:27:56 +0000

    Refactored to fix bug, return undefined when encounter error.

---
+++ asyncBreeds.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // ISSUE: Returning from inner callback function, not our main function.
+    console.log('Callback: I have the data!')
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    }
+  });
+  
+}
+
+module.exports = breedDetailsFromFile;
